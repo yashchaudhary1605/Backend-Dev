f0cc5b4e197555a6f5470c87ef6fd54941575f8d
yashchaudhary1605 <chaudharyyash64036@gmail.com>
2026-01-12 09:16:51 +0530

    [Yash] add:Module File

---
+++ modules/calculator.js
@@ -0,0 +1,6 @@
+// const {add, sub,multply,divide}=require('./operations') //common js type
+import {add, sub, multply, divide} from "./operations"; // module type
+add(4,5)
+sub(10,8)
+multply(4,5)
+divide(9,3)
---
+++ modules/operations.js
@@ -0,0 +1,15 @@
+const add=function(a,b){
+    console.log(a+b)
+}
+const sub=(a,b)=>{
+    console.log(a-b)
+}
+const multply=(a,b)=>{
+    console.log(a*b)
+}
+const divide=(a,b)=>{
+ console.log(a/b)
+}
+module.exports={
+    add,sub,multply,divide
+}
